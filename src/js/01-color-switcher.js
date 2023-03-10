8ff45e5e72d144e50c367b69f0f622c16ebc3b91
vsk2306 <khelemes.v.s@gmail.com>
2023-03-10 23:11:25 +0200

    up 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bgdBody = document.querySelector('body');
 
+let changeInterval = null;
+
+btnStart.addEventListener('click', () => {
+  changeInterval = setInterval(changeColor, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(changeInterval);
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+});
+
+function changeColor() {
+  bgdBody.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
